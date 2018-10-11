5ef692cf656a5ab9ea2d9fb5e4b4287c804e39a2
root <root@wz-st-live_edge-3.(none)>
2018-10-11 18:51:37 +0800

    first commit

--- conf/settings_production.py
+++ conf/settings_production.py
@@ -20,7 +20,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',  # 默认用mysql
         'NAME': APP_ID,                        # 数据库名 (默认与APP_ID相同)
-        'USER': '',                            # 你的数据库user
+        'USER': 'root',                            # 你的数据库user
         'PASSWORD': '@86Dj3Y(it',                        # 你的数据库password
         'HOST': '10.0.1.29',                   		   # 数据库HOST
         'PORT': '3306',                        # 默认3306
